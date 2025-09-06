1b0821144ca8de4fd423f2a3f396debc4c9b3a87
Youssoupha Sy <syyoussou630@gmail.com>
2025-09-06 22:45:49 +0000

    normalisation du nombre de jours au niveau de periode

--- daily_360/settings.py
+++ daily_360/settings.py
@@ -100,7 +100,8 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",  
     # "https://planner-dev-data-front.vercel.app",
-    "https://planner360.vercel.app"
+    # "https://planner360.vercel.app"
+    "https://planning360.vercel.app/"
 ]
 
 # Internationalization
